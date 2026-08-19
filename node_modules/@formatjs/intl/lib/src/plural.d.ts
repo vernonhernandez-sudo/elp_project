6e4d4d998b7fdb6c00e49798e91dc4263efc089c
vernonhernandez-sudo <vernonhernandez@explorabooks.com>
2026-08-20 03:35:06 +0800

    google files

--- node_modules/@formatjs/intl/lib/src/plural.d.ts
+++
@@ -1,5 +0,0 @@
-import { Formatters, IntlFormatters, OnErrorFn } from './types';
-export declare function formatPlural({ locale, onError, }: {
-    locale: string;
-    onError: OnErrorFn;
-}, getPluralRules: Formatters['getPluralRules'], value: Parameters<IntlFormatters['formatPlural']>[0], options?: Parameters<IntlFormatters['formatPlural']>[1]): Intl.LDMLPluralRule;
